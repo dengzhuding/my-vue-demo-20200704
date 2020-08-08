7db6688ad24a7849ca02588826299ba45345145d
dzd_global <dengzhuding@123.com>
2020-08-08 20:15:48 +0800

    test

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import axios from 'axios'
 // 配置axios
 const instance = axios.create({
   // baseURL: 'https://some-domain.com/api/',
+  // baseURL: 'http://192.168.120.17:8080/',
   timeout: 20000,
+  withCredentials: true,
   headers: {
     'X-Custom-Header': 'docker-node-proxy'
   }
