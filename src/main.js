e0744a2345936bf881d22d450081c9a510bf9e40
dzd_global <dengzhuding@123.com>
2020-08-08 19:18:47 +0800

    修改代理目标地址

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+// 配置axios
+const instance = axios.create({
+  // baseURL: 'https://some-domain.com/api/',
+  timeout: 20000,
+  headers: {
+    'X-Custom-Header': 'docker-node-proxy'
+  }
+})
+Vue.prototype.$axios = instance
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,29 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      hello: 'Hello world！',
+      requestData: null
+    }
+  },
+  mounted () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      this.$axios.get('/demo/get/1')
+        .then(res => {
+          this.requestData = res && res.data
+          alert(res.data && JSON.stringify(res.data))
+        })
+        .catch(err => {
+          console.error((err && err.message) || '请求错误！')
+        })
+    }
+  }
+}
+</script>
